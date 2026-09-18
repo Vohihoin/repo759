269d4120d8f648235d3c24afdce8c45ada584435
Vahe Ohihoin <vahe.oo@yahoo.com>
2026-09-18 12:04:25 -0500

    Added a HW01 file out

---
+++ HW01/task4.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+#SBATCH -p instruction
+
+#SBATCH --job-name=FirstSlurm
+#SBATCH --error=FirstSlurm.err
+#SBATCH --output=FirstSlurm.out
+
+#SBATCH --time=0-00:01:00
+#SBATCH --ntasks=1 --cpus-per-task=2
+
+# actual script commands
+hostname
